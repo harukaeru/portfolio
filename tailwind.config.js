8080b8e66933cf162d26d30e49d7352d887fe275
harukaeru <nekoneko.myaomyao@gmail.com>
2021-01-31 11:27:23 +0900

    2021/01/31 changed tailwindcss.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,12 +2,20 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        xl: {
+          css: {
+            'ul > li': {
+              paddingLeft: '1.3em',
+            },
+          },
+        },
+      },
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-  ],
+  plugins: [require('@tailwindcss/typography')],
 }
