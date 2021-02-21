df415170192ded60fc15537f508a3973e94f3afd
harukaeru <nekoneko.myaomyao@gmail.com>
2021-02-22 02:20:10 +0900

    Stop purging

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,5 @@
 module.exports = {
-  purge: {
-    enabled: process.env.NODE_ENV === 'production',
-    content: [
-      'components/**/*.vue',
-      'layouts/**/*.vue',
-      'pages/**/*.vue',
-      'plugins/**/*.js',
-      'nuxt.config.js'
-    ]
-  },
+  purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
     rotate: {
