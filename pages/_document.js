601e89786dc8787b7e9d0d2d65235e3993ca29ec
harukaeru <nekoneko.myaomyao@gmail.com>
2021-02-26 23:35:08 +0900

    2021/02/26 removed font-awesome

--- pages/_document.js
+++ pages/_document.js
@@ -9,12 +9,6 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link
-            rel="stylesheet"
-            href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"
-            integrity="sha512-HK5fgLBL+xu6dm/Ii3z4xhlSUyZgTT9tuc/hSrtw6uzJOvgRr2a9jyxxT1ely+B+xFAmJKVSTbpM/CuL7qxO8w=="
-            crossOrigin="anonymous"
-          />
           <script
             dangerouslySetInnerHTML={{
               __html: `
